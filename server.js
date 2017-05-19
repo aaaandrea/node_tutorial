30b7614555aeff89e0bd3950534d3ddcfd9c3445
Andrea del Rio <adelrio@gmail.com>
2017-05-19 13:25:45 -0700

    add new server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-let http = require("http"); // import Node.js core http module
-
-let onRequest = (request, response) => {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-};
-
-http.createServer(onRequest).listen(8888); // create server and listen for incoming requests
+// let http = require("http"); // import Node.js core http module
+//
+// let onRequest = (request, response) => {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// };
+//
+// http.createServer(onRequest).listen(8888); // create server and listen for incoming requests
 
 // OR
 
@@ -22,38 +22,38 @@ http.createServer(onRequest).listen(8888); // create server and listen for incom
 
 // OR
 
-// var http = require('http'); // Import Node.js core module
-//
-// var server = http.createServer(function (req, res) {   //create web server
-//     if (req.url == '/') { //check the URL of the current request
-//
-//         // set response header
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//
-//         // set response content
-//         res.write('<html><body><p>This is home Page.</p></body></html>');
-//         res.end();
-//
-//     }
-//     else if (req.url == "/student") {
-//
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<html><body><p>This is student Page.</p></body></html>');
-//         res.end();
-//
-//     }
-//     else if (req.url == "/admin") {
-//
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<html><body><p>This is admin Page.</p></body></html>');
-//         res.end();
-//
-//     }
-//     else
-//         res.end('Invalid Request!');
-//
-// });
-//
-// server.listen(5000); //6 - listen for any incoming requests
-//
-// console.log('Node.js web server at port 5000 is running..')
+var http = require('http'); // Import Node.js core module
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request
+
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // set response content
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/student") {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/admin") {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+
+    }
+    else
+        res.end('Invalid Request!');
+
+});
+
+server.listen(5000); //6 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..');
